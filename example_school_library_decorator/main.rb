b74d4c44d5b1e9da95c999963e0d9e84f1dd8a13
Mihndim Julius <mihndim2016@gmail.com>
2021-10-22 05:29:24 -0400

    Add method to create person

--- example_school_library_decorator/main.rb
+++ example_school_library_decorator/main.rb
@@ -18,6 +18,44 @@ class App
     end
   end
 
+  def create_person
+    print "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
+    # *If needed, ask for parameters for the option.
+    person_type = gets.chomp
+    if person_type != "1" && person_type != "2"
+      puts "Invalid option"
+      return
+    end
+
+    print "Age: "
+    # *If needed, ask for parameters for the option.
+    age = gets.chomp
+
+    print "Name: "
+    # *If needed, ask for parameters for the option.
+    name = gets.chomp
+
+    person =
+      if person_type == "1"
+        print "Has parent permission? [Y/N]: "
+        # *If needed, ask for parameters for the option.
+        parent_permission = gets.chomp
+        parent_permission = parent_permission.downcase == "y"
+
+        Student.new(age, name, parent_permission)
+      elsif person_type == "2"
+        print "Specialization: "
+        # *If needed, ask for parameters for the option.
+        specialization = gets.chomp
+
+        Teacher.new(age, specialization, name)
+      end
+
+    @people << person
+    puts "Person created successfully"
+  end
+
+
 
 
 end
