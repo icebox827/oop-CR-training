05296b191b204f240a079aac3352703121323c96
Mihndim Julius <mihndim2016@gmail.com>
2021-10-22 05:27:21 -0400

    Add methods to list books and people

---
+++ example_school_library_decorator/main.rb
@@ -0,0 +1,23 @@
+class App
+
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+  def list_books
+    @books.each do |book|
+      puts "Title: \"#{book.title}\", Author: #{book.author}"
+    end
+  end
+
+  def list_people
+    @people.each do |person|
+      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end
+
+
+
+end
