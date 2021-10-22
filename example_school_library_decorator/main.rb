f1af319b3a2b231f7bd141622843884744b8401e
Mihndim Julius <mihndim2016@gmail.com>
2021-10-22 05:32:05 -0400

    Add method to create rentals

--- example_school_library_decorator/main.rb
+++ example_school_library_decorator/main.rb
@@ -68,6 +68,45 @@ class App
     @books << Book.new(title, author)
     puts "Book created successfully"
   end
+
+   # *Create a rental.
+   def create_rental
+    puts "Select a book from the following list by number"
+    @books.each_with_index do |book, index|
+      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+    end
+    # *If needed, ask for parameters for the option.
+    book_index = gets.chomp.to_i
+
+    puts "\nSelect a person from the following list by number (not id)"
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    # *If needed, ask for parameters for the option.
+    person_index = gets.chomp.to_i
+
+    print "\nDate: "
+    # *If needed, ask for parameters for the option.
+    date = gets.chomp
+
+    @rentals << Rental.new(date, @books[book_index], @people[person_index])
+    puts "Rental created successfully"
+  end
+  
+  # *List all rentals for a given person id.
+  def list_rentals_for_person_id
+    print "ID of person: "
+    # *If needed, ask for parameters for the option.
+    id = gets.chomp.to_i
+
+    rentals = @rentals.filter {|rental| rental.person.id == id}
+    puts "Rentals:"
+    rentals.each do |rental|
+      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
+    end
+  end
+
+
   
 
 
